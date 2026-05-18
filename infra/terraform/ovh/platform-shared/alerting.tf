657a8ae3035421f2b6187548449fd48eeee0327f
Uladzimier Kuzniacou <ukuzniacou@chili-publish.com>
2026-05-18 18:32:48 +0200

    ops(ovh-platform-shared): set cost alert reporting cadence to 24h

--- infra/terraform/ovh/platform-shared/alerting.tf
+++ infra/terraform/ovh/platform-shared/alerting.tf
@@ -2,5 +2,5 @@ resource "ovh_cloud_project_alerting" "monthly_budget" {
   service_name      = var.project_id
   email             = var.alert_email
   monthly_threshold = var.monthly_threshold_eur
-  delay             = 3600
+  delay             = 86400
 }
